3de7ddbeddba030fbf047dd45397c3a8eee96cdc
Khairulnizam <dearkai311@gmail.com>
2026-06-08 11:05:56 -0400

    Set up Redux provider

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
 
+// Create a Redux store using configureStore from Redux Toolkit
 const store = configureStore({
+  // Define the root reducer object
   reducer: {
+    // 'cart' is the name of the slice in the store, and it is managed by cartReducer
     cart: cartReducer,
   },
 });
